8114055c1461a363210ba944ab3e5a7702086c98
Aidan Cunniffe <aidan@useoptic.com>
2023-02-27 07:51:38 -0500

    redirect

---
+++ pages/docs/document-existing-api.tsx
@@ -0,0 +1,8 @@
+import { useEffect } from "react";
+
+export default function () {
+  useEffect(() => {
+    window.location.href = "/docs/openapi/generate-from-traffic";
+  });
+  return <div></div>;
+}
