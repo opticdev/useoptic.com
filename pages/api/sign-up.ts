32138708e716660fa511590341ccb6f15e4c6b6c
Aidan Cunniffe <aidan@useoptic.com>
2023-03-09 08:31:50 -0500

    fix analytics

--- pages/api/sign-up.ts
+++ pages/api/sign-up.ts
@@ -1,15 +1,18 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import Analytics from "analytics-node";
-import * as process from "process";
 
 export default function (req: NextApiRequest, res: NextApiResponse) {
   try {
-    const analytics = new Analytics(process.env.SEGMENT_KEY || "", {
       // Handle errors thrown here
       errorHandler: (err) => {},
     });
-    analytics.track({ event: "marketing.sign_up_click" });
-  } catch (e) {}
+    analytics.track({
+      event: "marketing.sign_up_click",
+      anonymousId: new Date().getTime().toString(36),
+    });
+  } catch (e) {
+    console.log(e);
+  }
 
   res.redirect("https://app.useoptic.com");
 }
