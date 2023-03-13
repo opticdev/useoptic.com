9c076b75ccf397e80b19bd08386fd7886711f530
Aidan Cunniffe <aidan@useoptic.com>
2023-03-13 11:57:50 -0400

    fix segment login

--- pages/api/sign-up.ts
+++ pages/api/sign-up.ts
@@ -2,18 +2,14 @@ import { NextApiRequest, NextApiResponse } from "next";
 import Analytics from "analytics-node";
 
 export default function (req: NextApiRequest, res: NextApiResponse) {
-  try {
-    const analytics = new Analytics(process.env.SEGMENT_KEY || "", {
-      // Handle errors thrown here
-      errorHandler: (err) => {},
-    });
-    analytics.track({
-      event: "marketing.sign_up_click",
-      anonymousId: new Date().getTime().toString(36),
-    });
-  } catch (e) {
-    console.log(e);
-  }
+  const analytics = new Analytics(process.env.SEGMENT_KEY || "", {
+    // Handle errors thrown here
+    errorHandler: (err) => {},
+  });
+  analytics.track({
+    event: "marketing.sign_up_click",
+    anonymousId: new Date().getTime().toString(36),
+  });
 
   res.redirect("https://app.useoptic.com");
 }
