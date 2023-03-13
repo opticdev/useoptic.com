7eacc40171541b196e1acd9038bef01ab3af87b1
Aidan Cunniffe <aidan@useoptic.com>
2023-03-13 12:14:08 -0400

    submit

--- pages/api/sign-up.ts
+++ pages/api/sign-up.ts
@@ -1,15 +1,19 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import Analytics from "analytics-node";
 
-export default function (req: NextApiRequest, res: NextApiResponse) {
-  const analytics = new Analytics(process.env.SEGMENT_KEY || "", {
+export default async function (req: NextApiRequest, res: NextApiResponse) {
+  const analytics = new Analytics(process.env.SEGMENT_KEY!, {
     // Handle errors thrown here
-    errorHandler: (err) => {},
+    errorHandler: (err) => {
+      throw new Error(err);
+    },
   });
   analytics.track({
     event: "marketing.sign_up_click",
     anonymousId: new Date().getTime().toString(36),
   });
+
+  await analytics.closeAndFlush();
 
   res.redirect("https://app.useoptic.com");
 }
